dca598b6f469b6b76e208b902fc8cdd276dfdde0
ZhiquanX <x.zhiquan@gmail.com>
2023-05-15 21:33:30 +0800

    add theme class to body but not the app node so that we can cover the dialogs from the theme style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <router-view id="free-app" :class="`free-app theme-${store.theme || 'default'}`"/>
+  <router-view id="free-app" class="free-app full-height"/>
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, watchEffect, getCurrentInstance } from 'vue';
 import useAppStore from '@/stores/app';
 
 export default defineComponent({
   name: 'App',
   setup() {
+    const { proxy:vm } = getCurrentInstance();
     const store = useAppStore();
+
+    watchEffect(() => {
+      const theme = store.theme || vm.ctx.config.defaultTheme || 'default';
+      if (!theme) return;
+
+      if (!document.body.classList.contains(`theme-${theme}`)) {
+        document.body.classList.add(`theme-${theme}`);
+      }
+    })
 
     return {
       store,
